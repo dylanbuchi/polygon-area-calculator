f732487f4cfb479df9131b5c28b94c8506206dbc
Dylan Buchi <devybuchi@gmail.com>
2021-07-12 21:54:52 -0300

    refactor Rectangle class and add Square class

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
+
 class Rectangle:
+
     MAX_WIDTH_AND_HEIGHT = 50
+    IS_TOO_BIG_STRING = "Too big for picture."
 
     def __init__(self, width: int, height: int) -> None:
         self.width = width
@@ -21,18 +26,12 @@ class Rectangle:
         return (self.width**2 + self.height**2)**0.5
 
     def get_picture(self) -> str:
-        if self.width > 50 or self.height > 50:
-            return "Too big for picture"
-        picture = []
+        if self._is_too_big():
+            return self.IS_TOO_BIG_STRING
+        return self._create_picture()
 
-        for _ in range(self.height):
-            for _ in range(self.width):
-                picture.append("*")
-            picture.append("\n")
-        return ''.join(picture)
-
-    def get_amount_inside(self, shape) -> str:
-        ...
+    def get_amount_inside(self, shape: Rectangle):
+        return self.get_area() // shape.get_area()
 
     def _is_too_big(self) -> bool:
         return self.width > self.MAX_WIDTH_AND_HEIGHT or self.height > self.MAX_WIDTH_AND_HEIGHT
@@ -40,5 +39,25 @@ class Rectangle:
     def _get_class_name(self) -> str:
         return type(self).__name__.capitalize()
 
+    def _create_picture(self) -> str:
+        picture = []
+        for _ in range(self.height):
+            for _ in range(self.width):
+                picture.append("*")
+            picture.append("\n")
+        return ''.join(picture)
+
     def __repr__(self) -> str:
         return f"{self._get_class_name()}(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+    def __init__(self, side: int) -> None:
+        super().__init__(side, side)
+
+    def set_side(self, side: int) -> None:
+        super().set_height(side)
+        super().set_width(side)
+
+    def __repr__(self) -> str:
+        return super()._get_class_name() + f"(side={self.width})"
